9e88f0003de035390811e3b324d8fffd1e7ccdf1
Optimista <chicoptimista@gmail.com>
2024-09-24 18:00:18 +0800

    Restore keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+local quarto = require("quarto")
+quarto.setup()
+vim.keymap.set("n", "<leader>qp", quarto.quartoPreview, { desc = "Preview", silent = true, noremap = true })
+-- Markdown Preview
+vim.keymap.set("n", "<leader>mp", "<CMD>MarkdownPreview<CR>", { desc = "Preview markdown files in browser" })
